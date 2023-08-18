3cf3cdb0dcf84b7198276ef1b6e412f919d647b6
Aagam Jain <79409258+getlost01@users.noreply.github.com>
2023-08-18 12:49:49 +0530

    test google APIs with googel auth then create meet

--- server.js
+++ server.js
@@ -1,72 +1,125 @@
+// index.js
 const express = require('express');
 const { google } = require('googleapis');
-const { auth } = require('google-auth-library');
+const { OAuth2Client } = require('google-auth-library');
+const { default: axios } = require('axios');
+const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 8000;
-const SCOPES = ['https://www.googleapis.com/auth/calendar'];
+const PORT = 3000; 
 
-// Load your service account credentials
-const keyPath = './key.json'; // Replace with the actual path to your service account key file
+// Your credentials goes here
+const CLIENT_ID = 'your_client_id.apps.googleusercontent.com';
+const CLIENT_SECRET = 'your_client_secret';
+const REDIRECT_URI = 'http://localhost:3000/oauth2callback'; 
 
-// Initialize the Google Calendar API client
-const calendar = google.calendar({ version: 'v3' });
+// Your cors app configuration
+app.use(cors());
 
-// Middleware to handle JSON bodies
-app.use(express.json());
+// Google OAuth2 configuration
+const oauth2Client = new OAuth2Client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
+const customData = {
+  name: 'Aagam Jain',
+  email: 'test@gmail.com'
+};
 
-// Route to create a Google Meet event
-app.post('/create-meet', async (req, res) => {
+// Api for google Auth
+app.get('/auth/google', (req, res) => {
+  const authUrl = oauth2Client.generateAuthUrl({
+    access_type: 'offline', 
+    scope: [
+      'https://www.googleapis.com/auth/calendar', 
+    ],
+    state: JSON.stringify(customData),
+  });
+  res.redirect(authUrl);
+});
+
+
+// Api for creating google meet after auth with google
+app.get('/create-meet', async (req, res) => {
   try {
-    // Set up Google Calendar API client
-    const authClient = await auth.getClient({
-      keyFile: keyPath,
-      scopes: SCOPES,
-    });
-
+    const calendar = google.calendar({ version: 'v3', auth: oauth2Client });
+    console.log('oauth2Client.credentials', oauth2Client.credentials);
     var event = {
-        'summary': 'Google I/O 2015',
-        'location': '800 Howard St., San Francisco, CA 94103',
-        'description': 'A chance to hear more about Google\'s developer products.',
-        'start': {
-          'dateTime': '2023-09-28T09:00:00-07:00',
-          'timeZone': 'America/Los_Angeles',
+      'summary': 'Interview link test2',
+      'location': 'Google Meet',
+      'description': 'Meet with aaagam jain',
+      'start': {
+        'dateTime': '2023-09-28T09:00:00-07:00',
+        'timeZone': 'Asia/Kolkata',
+      },
+      'end': {
+        'dateTime': '2023-09-28T17:00:00-07:00',
+        'timeZone': 'Asia/Kolkata',
+      },
+      'recurrence': [
+        'RRULE:FREQ=DAILY;COUNT=2'
+      ],
+      'attendees': [
+        {'email': 'aagamjain.test1@gmail.com'},
+        {'email': 'aagamjain.test2@gmail.com'},
+      ],
+      'conferenceData': {
+        'createRequest': {
+          'requestId': 'tester123',
+          'conferenceSolutionKey': {
+            'type': 'hangoutsMeet',
+          },
         },
-        'end': {
-          'dateTime': '2023-09-28T17:00:00-07:00',
-          'timeZone': 'America/Los_Angeles',
-        },
-        'recurrence': [
-          'RRULE:FREQ=DAILY;COUNT=2'
+      },
+      'reminders': {
+        'useDefault': true,
+        'overrides': [
+          {'method': 'email', 'minutes': 24 * 60},
+          {'method': 'popup', 'minutes': 10},
         ],
-        'attendees': [
-          {'email': 'aagamjain573@gmail.com'},
-          {'email': 'aagamjain9945@gmail.com'},
-        ],
-        'reminders': {
-          'useDefault': false,
-          'overrides': [
-            {'method': 'email', 'minutes': 24 * 60},
-            {'method': 'popup', 'minutes': 10},
-          ],
-        },
-      };
+      },
+    };
 
-    // Insert the event into the calendar
     const response = await calendar.events.insert({
-      auth: authClient,
       calendarId: 'primary',
       resource: event,
       conferenceDataVersion: 1,
     });
 
-    res.json(response.data);
+    // console.log('Event created: ', response.data);
+
+    res.status(200).json({ message: 'Google Meet event created successfully' });
+
   } catch (error) {
     console.error('Error creating event: ', error.message);
-    res.status(500).json({ error: 'Error creating event' });
+    res.status(500).json({ error: 'Failed to create Google Meet event.' });
   }
 });
 
+
+// Api for google callback
+app.get('/oauth2callback', async (req, res) => {
+  try {
+    const { tokens } = await oauth2Client.getToken(req.query.code);
+    oauth2Client.setCredentials(tokens);
+    const { state } = JSON.parse(req.query.state);
+    console.log('state', state);
+    res.redirect(`http://localhost:3001?name=${state.name}&email=${state.email}`);
+  } catch (error) {
+    console.error('Error retrieving access token: ', error.message);
+    res.status(500).json({ error: 'Error retrieving access token' });
+  }
+});
+
+
+app.get("*", (req, res) => {
+  res.status(404).send({
+      message: "Page not found 404", 
+      endPoints: {
+          "auth": "/auth/google",
+          "create-meet": "/create-meet"
+      }
+  });
+});
+
+// Start the server
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
