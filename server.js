a966bd8c0ef7c2d8c471c7d2c150dd6aa9b6fc66
Aagam Jain <79409258+getlost01@users.noreply.github.com>
2023-08-18 12:51:29 +0530

    create a single api for creating an google meet link within one call

--- server.js
+++ server.js
@@ -94,17 +94,58 @@ app.get('/create-meet', async (req, res) => {
 });
 
 
-// Api for google callback
+// Api for google Auth callback for one time meet creation
 app.get('/oauth2callback', async (req, res) => {
+  const { code, state } = req.query;
   try {
-    const { tokens } = await oauth2Client.getToken(req.query.code);
+    console.log('state', JSON.parse(state));
+    const { tokens } = await oauth2Client.getToken(code);
     oauth2Client.setCredentials(tokens);
-    const { state } = JSON.parse(req.query.state);
-    console.log('state', state);
-    res.redirect(`http://localhost:3001?name=${state.name}&email=${state.email}`);
+    
+    const calendar = google.calendar({ version: 'v3', auth: oauth2Client });
+    var event = {
+      summary: 'Interview link test2',
+      location: 'Google Meet',
+      description: 'Meet with aaagam jain',
+      start: {
+        'dateTime': '2023-09-28T09:00:00-07:00',
+        'timeZone': 'Asia/Kolkata',
+      },
+      end: {
+        'dateTime': '2023-09-28T17:00:00-07:00',
+        'timeZone': 'Asia/Kolkata',
+      },
+      attendees: [
+        {'email': 'aagamjain573@gmail.com'},
+        {'email': 'aagamjain9945@gmail.com'},
+      ],
+      conferenceData: {
+        createRequest: {
+          requestId: '123456765+56143',
+          conferenceSolutionKey: {
+            type: 'hangoutsMeet',
+          },
+        },
+      },
+      reminders: {
+        useDefault: false,
+        overrides: [
+          {method: 'email', minutes: 24 * 60},
+          {method: 'popup', minutes: 10},
+        ],
+      },
+    };
+
+    const response = await calendar.events.insert({
+      calendarId: 'primary',
+      resource: event,
+      conferenceDataVersion: 1,
+    });
+
+    res.send({ "Authentication" : "successful!", "message": "You can close this page now.", "link": response.data.hangoutLink});
   } catch (error) {
-    console.error('Error retrieving access token: ', error.message);
-    res.status(500).json({ error: 'Error retrieving access token' });
+    console.error('Error authenticating', error);
+    res.status(500).send('Authentication error!');
   }
 });
 
